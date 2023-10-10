8d0275d39d1c03813042fae926648c217251d447
Thomas Jaggi <thomas@responsive.ch>
2023-10-10 13:00:15 +0200

    Migrate to PostCSS 8

--- src/index.js
+++ src/index.js
@@ -1,44 +1,55 @@
 /* Tooling
 /* ========================================================================== */
 
-// external tooling
-import postcss from 'postcss';
-
 // internal tooling
 import transpileDecl from './lib/transpile-decl';
 
 /* Inline SVGs
 /* ========================================================================== */
 
-export default postcss.plugin('postcss-svg-fragments', opts => (css, result) => {
-	// svg promises array
-	const promises = [];
+const plugin = opts => {
+	return {
+		postcssPlugin: 'postcss-svg',
+		prepare (result) {
+			// svg promises array
+			const promises = [];
 
-	// plugin options
-	const normalizedOpts = {
-		// additional directories to search for SVGs
-		dirs: 'dirs' in Object(opts) ? [].concat(opts.dirs) : [],
-		// whether to encode as utf-8
-		utf8: 'utf8' in Object(opts) ? Boolean(opts.utf8) : true,
-		// whether and how to compress with svgo
-		svgo: 'svgo' in Object(opts) ? Object(opts.svgo) : false
-	};
+			// plugin options
+			const normalizedOpts = {
+				// additional directories to search for SVGs
+				dirs: 'dirs' in Object(opts) ? [].concat(opts.dirs) : [],
+				// whether to encode as utf-8
+				utf8: 'utf8' in Object(opts) ? Boolean(opts.utf8) : true,
+				// whether and how to compress with svgo
+				svgo: 'svgo' in Object(opts) ? Object(opts.svgo) : false
+			};
 
-	// cache of file content and json content promises
-	const cache = {};
+			// cache of file content and json content promises
+			const cache = {};
 
-	// for each declaration in the stylesheet
-	css.walkDecls(decl => {
-		// if the declaration contains a url()
-		if (containsUrlFunction(decl)) {
-			// transpile declaration parts
-			transpileDecl(result, promises, decl, normalizedOpts, cache);
+			return {
+				Once (root) {
+					// for each declaration in the stylesheet
+					root.walkDecls(decl => {
+						// if the declaration contains a url()
+						if (containsUrlFunction(decl)) {
+							// transpile declaration parts
+							transpileDecl(result, promises, decl, normalizedOpts, cache);
+						}
+					});
+				},
+				async OnceExit () {
+					// wait for chained svg promises array
+					await Promise.all(promises);
+				}
+			};
 		}
-	});
+	}
+}
 
-	// return chained svg promises array
-	return Promise.all(promises);
-});
+plugin.postcss = true;
+
+export default plugin;
 
 /* Inline Tooling
 /* ========================================================================== */
